c306cfa77bdbdb0016bb684baaff259962dad4bf
Dfrmn <50831819+Dfrmn@users.noreply.github.com>
2021-06-18 13:40:17 +0300

    Add files via upload

---
+++ Task1/script.js
@@ -0,0 +1,6 @@
+console.log("Boolean(Number('10')) + 1" , typeof( Boolean(Number('10')) + 1));
+console.log("'sub ' + Number(false)" , typeof ('sub ' + Number(false)));
+console.log("16  *  '      91    '" , typeof (16  *  '      91    '));
+console.log("true-70" , typeof (true-70)) ;
+console.log("Number(1 + String(1)) + 1" , typeof (Number(1 + String(1)) + 1));
+//console.log(Boolean(Number('10')) + 1, 'sub ' + Number(false), 16  *  '      91    ', true-70, Number(1 + String(1)) + 1);
---
+++ Task2/script.js
@@ -0,0 +1,3 @@
+let numberOfHours = parseInt(prompt('enter the number of hours'));
+const secondsPerHour = 3600;
+alert('Number of seconds in ' + numberOfHours + ' hours: ' + secondsPerHour * numberOfHours);
